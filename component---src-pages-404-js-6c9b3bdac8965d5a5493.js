b020607292853ee487082ed436c31f68fb8042b9
kis0421 <kig0421@gmail.com>
2022-01-03 22:40:23 +0900

    repo reset

--- component---src-pages-404-js-6c9b3bdac8965d5a5493.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkkis0421_github_io=self.webpackChunkkis0421_github_io||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),i=a(7198),u=a(3751);e.default=function(t){var e=t.data,a=t.location,l=e.site.siteMetadata.title;return n.createElement(i.Z,{location:a,title:l},n.createElement(u.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-6c9b3bdac8965d5a5493.js.map
