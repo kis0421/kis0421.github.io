8a578721c14c40eb40cc17e4c3b29da99e5af8bc
kis0421 <kig0421@gmail.com>
2022-01-04 00:01:10 +0900

    Updates

---
+++ component---src-pages-page-2-js-b7fa0d23e93e47ac2f21.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkis0421_github_io=self.webpackChunkkis0421_github_io||[]).push([[617],{9863:function(e,t,n){n.r(t);var l=n(7294),a=n(5444),c=n(5127),o=n(3751);t.default=function(){return l.createElement(c.Z,null,l.createElement(o.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(a.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-b7fa0d23e93e47ac2f21.js.map
