cf13d3acbc13c799dd12b1bdb8df6dabc15e8d3a
kis0421 <kig0421@gmail.com>
2022-01-04 23:03:57 +0900

    js파일 타입스크립트로 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,41 +2,39 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
+const Header = (props: { siteTitle }) => {
+  const { siteTitle } = props;
+  return <header
     style={{
       background: `rebeccapurple`,
       marginBottom: `1.45rem`,
-    }}
-  >
+    }}>
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
-      }}
-    >
+      }}>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
             color: `white`,
             textDecoration: `none`,
-          }}
-        >
+          }}>
           {siteTitle}
         </Link>
       </h1>
     </div>
   </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
+
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
 
 export default Header
