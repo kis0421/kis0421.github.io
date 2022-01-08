b012cff54c0ce86a92568a09bae94b94e918eb31
김민규 <kig0421@gmail.com>
2022-01-08 15:22:54 +0900

    메인페이지에서 최근 글 리스트 불러오도록

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,45 @@
 import * as React from "react"
-import { Link, useStaticQuery } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-// import { latestPostList } from "../lib/getPages"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-/*
-TODO: 메인페이지 글 리스트 불러오기
- allFile(filter: {extension: {eq: "md"}}) {
-    edges {
-      node {
-        name
-        extension
-        birthTime
-        dir
-        childrenMarkdownRemark {
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+      edges {
+        node {
+          id
+          excerpt(truncate: true, pruneLength: 200)
           frontmatter {
             title
+            contentsType
+            date
           }
         }
       }
     }
   }
-*/
-const IndexPage = () => {
-  // const data = useStaticQuery(latestPostList);
-  // console.log(data)
+ `);
   return <Layout>
     <Seo title="Home" />
     <h1>Hi people~</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+
+
+    {data.allMarkdownRemark.edges.map(({ node }) => {
+
+      return <div key={node.id}>
+        <h2>{node.frontmatter.title}</h2>
+        <p>{node.excerpt}</p>
+        <div style={{ color: "#ababab" }}>{node.frontmatter.date}</div>
+        <hr />
+      </div>
+    })}
+
     {/* 
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
