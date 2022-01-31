165fa6c9759d89e3f2657be9ff649d2915b69e5c
kis0421 <kig0421@gmail.com>
2022-01-31 20:26:25 +0900

    포스팅한 글 페이지도 다크모드 적용

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -33,7 +33,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
-        <main>{children}</main>
+        <main className="templateWrap">{children}</main>
         {/* <footer
           style={{
             marginTop: `2rem`,
