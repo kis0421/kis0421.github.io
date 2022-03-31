1634299712584a030d05127e5c744112fa4f820c
kis0421 <kig0421@gmail.com>
2022-04-01 06:24:08 +0900

    코드 하이라이트  설정

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,24 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: true,
+              noInlineHighlight: false,
+              
+            },
+          },
+        ],
+      },
+    },
     `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
--- src/templates/PostTemplate.tsx
+++ src/templates/PostTemplate.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Prism from "prismjs";
 import Layout from "../components/layout"
 
-const PostTemplate: React.FC = (props) => {
 
+const PostTemplate: React.FC = (props) => {
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [])
   return (<Layout>
     <div dangerouslySetInnerHTML={{ __html: props.pageContext.html }}></div>
-
   </Layout>)
 }
 export default React.memo(PostTemplate);
