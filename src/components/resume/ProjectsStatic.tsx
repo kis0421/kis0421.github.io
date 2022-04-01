6eece0ac8efc5de753d53c0b5e8159a9bbf6a183
kis0421 <kig0421@gmail.com>
2022-04-02 00:56:35 +0900

    프로젝트 임시 커밋

--- src/components/resume/ProjectsStatic.tsx
+++ src/components/resume/ProjectsStatic.tsx
@@ -2,10 +2,10 @@ import * as React from "react"
 
 const ProjectsStatic: Array<{
   company: string;
-  title: string | Element;
+  title: string | JSX.Element;
   date: string;
   projectMember: string;
-  content: string | Element;
+  content: string | JSX.Element;
   keyword: string[];
 }> = [
     {
@@ -13,15 +13,32 @@ const ProjectsStatic: Array<{
       title: "온라인 유통 판매 B2B 웹 사이트",
       date: "2015.09 - 2017.06",
       projectMember: "본인 외 2인",
-      content: "만드는중",
+      content: <>
+        <div></div>
+        <div></div>
+      </>,
       keyword: ["php"]
     },
     {
       company: "(주)오너클랜",
-      title: "오픈마켓 상품관리 데스크탑 애플리케이션",
+      title: "오픈마켓 상품 관리 데스크탑 애플리케이션",
       date: "2018.9 ~ 2021.04",
       projectMember: "본인 외 1인",
-      content: "만드는중",
+      content: <>
+        <div></div>
+        <div></div>
+      </>,
+      keyword: ["php"]
+    },
+    {
+      company: "(주)차이몰",
+      title: "B2B 서비스 웹 개발",
+      date: "2021.05 ~ 2022.01",
+      projectMember: "본인 외 1인",
+      content: <>
+        <div></div>
+        <div></div>
+      </>,
       keyword: ["php"]
     },
   ];
