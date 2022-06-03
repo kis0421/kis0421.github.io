a9fb07de518be71e7a006d00b49210ee90482c98
김민규 <kig0421@gmail.com>
2022-06-03 20:11:17 +0900

    Updates

--- component---src-pages-404-js-65d9b227127167231def.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkkis0421_github_io=self.webpackChunkkis0421_github_io||[]).push([[883],{9616:function(e,t,n){n.r(t);var u=n(7294),l=n(5127),s=n(3751);t.default=function(){return u.createElement(l.Z,null,u.createElement(s.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-65d9b227127167231def.js.map
